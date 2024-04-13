f8879f9e24fa709ada303d60c1fb1f9af2f7a757
Aditya <4adi26@gmail.com>
2024-04-13 11:12:01 +0530

    setup clerk auth and router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUpPage'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route index="true" path='/' element={<Dashboard />} />
+      <Route path='/sign-in' element={<SignInPage />} />
+      <Route path='/sign-up' element={<SignUpPage />} />
+    </Routes>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{variables: {colorPrimary: '#624cf5'}}} signInUrl='/sign-in' signUpUrl='/sign-up'>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>,
 )
